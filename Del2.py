7fc0af1a2fb522a4ab9c7c062bae6cf9ede7ef2b
meow <114763852+eofieoakfvoa@users.noreply.github.com>
2025-04-20 05:44:17 +0200

    :cat:

---
+++ Del2.py
@@ -0,0 +1,23 @@
+
+def main():
+    running = True
+    while running:
+        print("Skriv [1] för att lägga till en bok \n [2] för att få alla böcker och deras information")
+
+
+
+
+
+
+
+
+
+#class
+
+class Bok:
+    def __init__(self, title):
+        self.Title = title
+        self.Author = author
+        self.pageCount = pagecount
+        self.Price = price
+main()
